4adb0207ec4f0dfc15d787f258bc634940c66853
Amece <resoros@outlook.com>
2021-01-20 10:36:45 +0100

    Session code in een apart bestand zetten

--- login.php
+++ login.php
@@ -1,12 +1,6 @@
 <?php
-// Initialize the session
-session_start();
-
-// Check if the user is already logged in, if yes then redirect him to welcome page
-if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-    header("location: overzicht.php");
-    exit;
-}
+// Include the session file
+require_once 'php/session.php';
 
 // Include config file
 /** @var mysqli $mysqli */
