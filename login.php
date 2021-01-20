b555e2b2720135861e399d85ea169db56b1edf1f
Amece <resoros@outlook.com>
2021-01-20 10:59:49 +0100

    De verkeerde code voor session veranderd en weer werkend gekregen

--- login.php
+++ login.php
@@ -1,6 +1,12 @@
 <?php
-// Include the session file
-require_once 'php/session.php';
+// Initialize the session
+session_start();
+
+// Check if the user is already logged in, if yes then redirect him to welcome page
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+    header("location: overzicht.php");
+    exit;
+}
 
 // Include config file
 /** @var mysqli $mysqli */
